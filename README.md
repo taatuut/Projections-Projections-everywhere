f1d06d5e8bb06960e17893e194b2b9cf77038ef8
Emil Zegers <e.zegers@upcmail.nl>
2023-06-26 14:25:28 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# Projections-Projections-everywhere
-Projections Projections everywhere
+# Projections Projections everywhere
+
+![Alt text](images/projections.png?raw=true "Projections Projections everywhere")
+
+## Purpose
+
+Option for more efficient coordinate reprojection for geospatial data not in WGS84.
+
+Removes dependency on 'serial feature conversion using a webservice' being error prone and causing overhead and delay.
+
+## Prerequisites
+
+Recent `Python3` with `Pandas` and `odfpy`.
+
+```
+python3 -m pip install --upgrade pip
+python3 -m pip install pandas odfpy
+```
+
+## Steps
+
+In this example I start with Dutch RD-coördinaatpunten data from https://www.nsgi.nl/documents/1888506/3754578/20220111+Lijst+actuele+kernnetpunten+voor+RDinfo.ods/ae6fecb4-e145-8d93-8a91-fc578dd18d52?t=1641991881508
+This file comes in OpenDocument Spreadsheet (ods) file format [1]. More common is probably getting csv or Esri Shapefile as input.
+
+1.
+Create csv from ods file
+
+```
+python3 ods2csv.py
+```
+
+Result is file `input.csv`.
+
+2.
+Transform RD EPSG:28992 to UTM Zone 32N EPSG:23032 and WGS84 EPSG:4326
+
+```
+python3 ogr_proj.py
+```
+
+Result is file `output.csv`.
+
+NOTE: Very important is not to do 'serial reprojections'!! E.g. 28992 to 23032 to 4326. Determine original Coordinate Reference System (CRS)for source, in this case that is RD or EPSG:28992, ETRS89 is derived CRS here (a nice read is on this topic is 'De geodetische referentiestelsels van Nederland - Geodetic reference frames in the Netherlands' [5]). Use the source CRS every time for conversion to another system. E.g. 28992 to 23032, 28992 to 4326 etc. Also check how many deciamls make sense in the resulting output.
+
+3.
+TODO: Describe role of vrt file
+
+4.
+Could do further processing like convert to geojson, ...
+
+## Check result file
+
+Go to https://geojson.io/ [2]
+
+Click [ Open ] and select `output.csv`. Will load the file assuming `lat` and `lon` to contain WGS84 coordinates. Can export to geojson and more.
+
+![Alt text](images/geojson.io.1.png?raw=true "geojson.io")
+
+![Alt text](images/geojson.io.2.png?raw=true "kernnetpunten rijksdriehoekmeting")
+
+## Links
+
+[1]
+https://www.nsgi.nl/geodetische-infrastructuur/referentiestelsels/rdinfo
+
+[2]
+https://geojson.io/
+
+[3]
+https://geojson.tools/
+
+[4]
+https://geojsonlint.com/
+
+[5]
+https://ncgeo.nl/downloads/43Referentie.pdf
---
+++ ogr_proj.py
@@ -0,0 +1,66 @@
+from osgeo import ogr, osr
+import csv
+
+# Define the input and output file paths
+input_file = 'input.vrt'
+output_file = 'output.csv'
+
+# Create a spatial reference for EPSG:28992 (RD New)
+source_sr_28992 = osr.SpatialReference()
+source_sr_28992.ImportFromEPSG(28992)
+
+# Create a spatial reference for EPSG:23032 (UTM Zone 32N)
+target_sr_23032 = osr.SpatialReference()
+target_sr_23032.ImportFromEPSG(23032)
+
+# Create a spatial reference for EPSG:4326 (WGS84)
+target_sr_4326 = osr.SpatialReference()
+target_sr_4326.ImportFromEPSG(4326)
+
+# Create a coordinate transformation from source to target
+transform_28992_23032 = osr.CoordinateTransformation(source_sr_28992, target_sr_23032)
+transform_28992_4326 = osr.CoordinateTransformation(source_sr_28992, target_sr_4326)
+
+# Open the input VRT file
+input_dataset = ogr.Open(input_file)
+input_layer = input_dataset.GetLayer()
+
+# Get the field names from the input layer
+field_names = [field.GetName() for field in input_layer.schema]
+
+# Create the output CSV file and write the header
+output_dataset = open(output_file, 'w', newline='')
+csv_writer = csv.writer(output_dataset, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+csv_writer.writerow(field_names + ['x', 'y', 'lon', 'lat'])
+
+# Iterate over features in the input layer
+# use separate 'split' features for two instances of the input layer
+# cannot 'reuse' feature for multipe transform operations, messes up result
+# also cannot use module copy because feature is not supported for for deepcopy  
+for feature_23032, feature_4326 in zip(input_layer, input_layer):
+    # Get the attributes from the input feature
+    # Use any of the feature_* here to get the field names
+    attributes = [feature_23032.GetField(field_name) for field_name in field_names]
+
+    # Get the geometry from the input feature
+    geometry_28992_23032 = feature_23032.GetGeometryRef()
+    geometry_28992_4326 = feature_4326.GetGeometryRef()
+
+    # Transform the geometry to the target spatial reference
+    geometry_28992_23032.Transform(transform_28992_23032)
+    geometry_28992_4326.Transform(transform_28992_4326)
+
+    # Get the transformed coordinates
+    x = geometry_28992_23032.GetX()
+    y = geometry_28992_23032.GetY()
+    lat = geometry_28992_4326.GetX()
+    lon = geometry_28992_4326.GetY()
+
+    # Write the transformed feature to the output CSV file
+    csv_writer.writerow(attributes + [x, y, lon, lat])
+
+# Close the datasets
+input_dataset = None
+output_dataset.close()
+
+print("Reprojection completed.")
